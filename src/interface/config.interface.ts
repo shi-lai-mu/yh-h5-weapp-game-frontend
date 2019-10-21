214c32caa00cc97945a57246cf170b54859b53c6
shi-lai-mu <478889187@qq.com>
2019-10-21 11:17:30 +0800

    feat: 构建项目

---
+++ src/interface/config.interface.ts
@@ -0,0 +1,15 @@
+export interface DefaultConfig {
+  /**
+   * 服务器配置
+   */
+  server: {
+    /**
+     * ip
+     */
+    host: string;
+    /**
+     * 接口
+     */
+    api: string;
+  };
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+  },
+})
+export default class Home extends Vue {}
+</script>
