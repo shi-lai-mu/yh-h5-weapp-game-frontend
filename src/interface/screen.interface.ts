2c0b0c7f741b9be16f75604d1d4bf39b51c31b8a
shi-lai-mu <478889187@qq.com>
2019-10-23 23:11:31 +0800

    feat: 封装设置强制横屏方法

---
+++ src/interface/screen.interface.ts
@@ -0,0 +1,8 @@
+export interface ScreenInterface {
+  'width': string;
+  'height': string;
+  'top': string;
+  'left': string;
+  'transform': string;
+  'transform-origin': string;
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="home">
+  <div :style="home" class="home">
+    <img src="https://yanxuan.nosdn.127.net/14943267735961674.jpg" alt="">
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import screen from '@/utils/screen';
+import { ScreenInterface } from '@/interface/screen.interface';
 
 @Component({
   components: {
   },
 })
 export default class Home extends Vue {
-  public created() {
-    console.log(this);
+  public home = {};
+  public mounted() {
+    const landscape: any = screen();
+    this.home = landscape;
   }
 }
 </script>
+<style lang="scss" scoped>
+  .home {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  img {
+    width: 100vh;
+    height: 100vw;
+  }
+</style>
