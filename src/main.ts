23e0667a38c86e18e2139ca6f0ffadeb6bfb82c7
yuze <17630918513@163.com>
2019-10-30 22:33:31 +0800

    feat: 登陆账号，vuex数据分离

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/index';
 import axios from '@/lib/axios';
 import Vant from 'vant';
 import 'vant/lib/index.css';
---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import state from './state';
+import modules from './modules';
+import mutations from './mutations';
+import actions from './actions';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  modules,
+});
