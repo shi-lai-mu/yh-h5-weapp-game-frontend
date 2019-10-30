a33da30689dab33286e373892e7cd77c8415cee8
yuze <17630918513@163.com>
2019-10-30 22:44:37 +0800

    test: 测试登陆

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -163,6 +163,7 @@ export default class Login extends Vue {
       }).then( (res: any) => {
         Toast.clear();
         if (res.id) {
+          console.log(res);
           Toast('注册成功');
           setTimeout(() => {
             this.$router.push({
