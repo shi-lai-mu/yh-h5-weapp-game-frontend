a9c88c65aaf9e098f06bc91009159a89f179af43
shi-lai-mu <478889187@qq.com>
2019-12-30 22:54:00 +0800

    feat: 添加邮件入口, 组件面板样式2

---
+++ src/views/Home/email.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    settingsad中文
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator';
+
+@Component
+export default class HomeSetting extends Vue {
+  private created() {
+    console.log(123465);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  
+</style>
