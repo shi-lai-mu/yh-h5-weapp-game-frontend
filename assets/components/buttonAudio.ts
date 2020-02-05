acb741b8be0cd2e3c62d67aa28066b20527a9151
shi-lai-mu <478889187@qq.com>
2020-02-05 16:48:15 +0800

    feat: 邮件页面开发

---
+++ assets/components/buttonAudio.ts
@@ -0,0 +1,29 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class ButtonAudio extends cc.Button {
+
+    @property({
+        type: cc.AudioClip,
+    })
+    clip: cc.AudioClip = null;
+
+    _onTouchEnded(event: any) {
+        if (!this.interactable || !this.enabledInHierarchy) return;
+        cc.audioEngine.play(this.clip, false, 1);
+        cc.Component.EventHandler.emitEvents(this.clickEvents, event);
+        this.node.emit('click', this);
+        event.stopPropagation();
+    }
+    // update (dt) {}
+}
