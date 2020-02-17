afa1cac06035d8a12367daeb768274c51067eec5
shi-lai-mu <478889187@qq.com>
2020-02-17 20:05:50 +0800

    feat: 结算资源细化初始

--- assets/scripts/utils/tool.ts
+++ assets/scripts/utils/tool.ts
@@ -15,7 +15,6 @@ export default {
         callback?: (targetPack: string, successCount: number, allCount: number) => boolean
     ) {
         if (Object.keys(cc.loader.downloader._subpackages).length === 0) {
-            console.log('跳过 subPackage 加载...')
             return callback && callback('skip', 1, 1);
         }
 
@@ -24,16 +23,12 @@ export default {
 
         for (const subIndex in sub) {
             const subName = sub[subIndex];
-            console.log(subName + ' subPackage 加载...')
             await new Promise((resolve) => {
-                console.time();
                 cc.loader.downloader.loadSubpackage(subName, (err) => {
                     if (err) {
                         return console.error(err);
                     }
-                    console.log(subName + ' 加载成功!');
                     resolve();
-                    console.timeEnd();
                     callback && callback(subName, Number(subIndex) + 1, allCount);
                 });
             })
@@ -41,19 +36,54 @@ export default {
 
         for (const sceneIndex in scene) {
             const sceneName = scene[sceneIndex];
-            console.log(sceneName + ' scene 加载...')
             await new Promise((resolve) => {
-                console.time();
                 cc.director.preloadScene(sceneName, (err) => {
                     if (err) {
                         return console.error(err);
                     }
-                    console.log(sceneName + ' 加载成功!');
                     resolve();
-                    console.timeEnd();
                     callback && callback(sceneName, Number(sceneIndex) + 1 + sub.length, allCount);
                 });
             })
         }
+    },
+
+
+    /**
+     *  格式化日期
+     *  @param fmt 日期格式 如：yyyy-MM-dd HH:mm:ss
+     *  @param form 指定时间 不传参 默认目前时间
+     */
+    dateFrom(fmt: string = 'yyyy-MM-dd HH:mm:ss', form?: number) {
+        const date: any = form ? new Date(form) : new Date();
+        const o: any = {
+            'M+': date.getMonth() + 1,
+            'd+': date.getDate(),
+            'h+': date.getHours() % 12 === 0 ? 12 : date.getHours() % 12,
+            'H+': date.getHours(),
+            'm+': date.getMinutes(),
+            's+': date.getSeconds(),
+            'q+': Math.floor((date.getMonth() + 3) / 3),
+            'S': date.getMilliseconds(),
+        };
+        const week: string[] = [ '日', '一', '二', '三', '四', '五', '六' ];
+        const season: string[] = [ '', '春', '夏', '秋', '冬' ];
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+        }
+        if (/(E+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? '星期' : '周') : '')
+            + week[date.getDay()]);
+        }
+        if (/(q+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, season[o['q+']] + ((RegExp.$1.length > 1) ? '季' : ''));
+        }
+        let k: string;
+        for (k in o) {
+            if (new RegExp('(' + k + ')').test(fmt)) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+            }
+        }
+        return fmt;
     },
 }
