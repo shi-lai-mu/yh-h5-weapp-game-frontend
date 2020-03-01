76519cf464a03d750a453bb862d136dbe87e8b58
shi-lai-mu <478889187@qq.com>
2020-03-01 21:21:41 +0800

    feat: 扑克牌换行机制

--- assets/scripts/Games/fourCards/main.ts
+++ assets/scripts/Games/fourCards/main.ts
@@ -51,7 +51,51 @@ export default class FourCardsGame extends cc.Component {
     // onLoad () {}
 
     start () {
+        // 模拟发牌
+        const { node } = this;
+        const screenWidth = node.width;
+        const screenHeight = node.height;
+        const colorKey = Object.keys(this.Card);
+        // 主颜色
+        let mainColor = 0;
+        // 子颜色
+        let mainChildColor = 0;
+        // 断点行
+        let startX = 0;
+        for (let i = 0; i < 54; i++) {
+            const targetFrame = this.Card[colorKey[mainColor]][mainChildColor];
+            const newNode = new cc.Node();
+            const nodeSprice = newNode.addComponent(cc.Sprite);
+            nodeSprice.spriteFrame = targetFrame;
+            // 30: 每张牌可见距离， 0.5: 屏幕左侧开始  100: 安全距离
+            newNode.x = i * 30 - (screenWidth * .5) + 100;
+            newNode.y -= (screenHeight * .5 - (newNode.height * .5));
 
+            // 一行占满 换行判断
+            if (newNode.x >= screenWidth * .5 - 100) {
+                // 断点开始换行
+                if (!startX) startX = i;
+                newNode.x -= startX * 30;
+                // 60为往下
+                newNode.y -= 60;
+                // 三行判断
+                if (newNode.x >= screenWidth * .5 - 100) {
+                    newNode.x -= startX * 30;
+                    newNode.y -= 60;
+                }
+            }
+            
+            // 三行判断
+
+            this.node.addChild(newNode);
+
+            // 全拍展示 【测试案例】
+            mainChildColor++;
+            if (mainChildColor === this.Card[colorKey[mainColor]].length) {
+                mainColor++;
+                mainChildColor = 0;
+            }
+        }
     }
 
     // update (dt) {}
