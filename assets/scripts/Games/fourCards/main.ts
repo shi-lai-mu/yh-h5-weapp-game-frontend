95caf74b8833070164b13dafe09f825a48430455
shi-lai-mu <478889187@qq.com>
2020-03-01 18:20:19 +0800

    feat: 扑克牌绑定

--- assets/scripts/Games/fourCards/main.ts
+++ assets/scripts/Games/fourCards/main.ts
@@ -9,18 +9,42 @@
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
 const {ccclass, property} = cc._decorator;
-
+const CardItem = cc.Class({
+    name: 'cardItem',
+    properties: {
+        plum: [ cc.SpriteFrame ],
+        heart: [ cc.SpriteFrame ],
+        block: [ cc.SpriteFrame ],
+        Spade: [ cc.SpriteFrame ],
+        joker: [ cc.SpriteFrame ],
+    }
+});
 @ccclass
-export default class NewClass extends cc.Component {
+export default class FourCardsGame extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
-    @property()
-    Card: { [key: string]: cc.Node[]} = {}
+    @property(CardItem)
+    Card = {
+        /**
+         * 梅花
+         */
+        plum: [],
+        /**
+         * 红心
+         */
+        heart: [],
+        /**
+         * 方块
+         */
+        block: [],
+        /**
+         * 黑桃
+         */
+        Spade: [],
+        /**
+         * 大小王
+         */
+        joker: [],
+    }
 
     // LIFE-CYCLE CALLBACKS:
 
