090f0f5ae5021dd801109fccd18e08c459e0fb36
shi-lai-mu <478889187@qq.com>
2020-03-31 19:10:54 +0800

    feat: 个人中心优化

--- assets/scripts/Home/user/userCenter.ts
+++ assets/scripts/Home/user/userCenter.ts
@@ -13,45 +13,24 @@ import State from '../../utils/state';
 
 @ccclass
 export default class NewClass extends cc.Component {
+    // 账号id
+    @property(cc.Label) id: cc.Label = null;
+    // 昵称
+    @property(cc.Label) nickName: cc.Label = null;
+    // 头像
+    @property(cc.Sprite) avatar: cc.Sprite = null;
+    // gender man
+    @property(cc.Sprite) man: cc.Sprite = null;
+    // gender woman
+    @property(cc.Sprite) woman: cc.Sprite = null;
+    // 账号
+    @property(cc.Label) account: cc.Label = null;
+    // 手机号
+    @property(cc.Label) mobile: cc.Label = null;
 
-    /**
-     * 账号id
-     */
-    @property(cc.Label)
-    id: cc.Label = null;
-
-    /**
-     * 昵称
-     */
-    @property(cc.Label)
-    nickName: cc.Label = null;
-
-    /**
-     * 头像
-     */
-    @property(cc.Sprite)
-    avatar: cc.Sprite = null;
-
-    /**
-     * gender man
-     */
-    @property(cc.Sprite)
-    man: cc.Sprite = null;
-
-    /**
-     * gender woman
-     */
-    @property(cc.Sprite)
-    woman: cc.Sprite = null;
-
-    /**
-     * 账号
-     */
-    @property(cc.Label)
-    account: cc.Label = null;
 
     start () {
-        const { nickname, id, avatarUrl, account, gender } = State.userInfo;
+        const { nickname, id, avatarUrl, account, gender, mobile } = State.userInfo;
         this.nickName.string = nickname;
         this.id.string = 'ID: ' + (id || '000000').toString();
         this.account.string = account.toString();
@@ -60,9 +39,14 @@ export default class NewClass extends cc.Component {
         gender === 1
             ? (this.woman.spriteFrame = this.man.spriteFrame) && (this.man.spriteFrame = emptyRadio)
             : null;
+
+        this.mobile.string = mobile;
     }
 
 
+    /**
+     * 加载头像
+     */
     loadAvatarUrl(id, avatarUrl) {
         // avatarUrl === 1 时加载ID的头像否则加载Default头像
         cc.loader.load(`https://perfergame.oss-cn-beijing.aliyuncs.com/avatar/${avatarUrl ? id : 'default'}.png`, (error, texture) => {
@@ -70,6 +54,24 @@ export default class NewClass extends cc.Component {
             var spriteFrame = new cc.SpriteFrame(texture);
             this.avatar.spriteFrame = spriteFrame;
         });
+    }
+
+
+    /**
+     * 退出登录
+     */
+    outLogin() {
+        State.userInfo = {
+            id: '0000',
+            nickname: '* * * * * *',
+            diamond: 0,
+            gold: 0,
+            token: false,
+            avatarUrl: false,
+            signal: 0,
+        };
+        localStorage.clear();
+        cc.director.loadScene('loginPage');
     }
 
     
