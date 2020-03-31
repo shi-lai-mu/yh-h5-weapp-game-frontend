9fd21a49418e008cc7ab5e2e867fcbd3cb2c56a1
shi-lai-mu <478889187@qq.com>
2020-03-31 15:57:08 +0800

    feat: 登录代码维护优化

---
+++ assets/scripts/loginPage/resetPassword.ts
@@ -0,0 +1,29 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+
+    /**
+     * 关闭登录按钮
+     */
+    onClose() {
+        this.node.destroy();
+    }
+
+    // update (dt) {}
+}
