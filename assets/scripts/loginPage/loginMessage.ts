4344c7ff71afcd0a1dd4cad4ffb956d30d79b4c4
shi-lai-mu <478889187@qq.com>
2020-04-05 11:36:35 +0800

    feat: 1. 测试人员文字 2. 修复个人中心头像加载失败 3. 修复退出登录后io依然连着 4. 优化游戏脚本

---
+++ assets/scripts/loginPage/loginMessage.ts
@@ -0,0 +1,15 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass} = cc._decorator;
+
+@ccclass
+export default class LoginMessage extends cc.Component {
+    close() {
+        this.node.destroy();
+    }
+}
