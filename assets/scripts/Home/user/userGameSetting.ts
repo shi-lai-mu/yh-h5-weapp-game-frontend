c111a725002b72465b9b56e56d29c3ee6f9c85e0
shi-lai-mu <478889187@qq.com>
2020-04-27 17:41:56 +0800

    feat: 初始化设置

--- assets/scripts/Home/user/userGameSetting.ts
+++ assets/scripts/Home/user/userGameSetting.ts
@@ -7,20 +7,62 @@
 
 const {ccclass, property} = cc._decorator;
 import State from '../../utils/state';
+import Toggle from '../../perfab/script/Radio/toggle';
 
 @ccclass
 export default class UserGameSetting extends cc.Component {
-    // 音量调节
-    @property(cc.ProgressBar) volume: cc.ProgressBar = null;
+    // 音乐节点
+    @property(cc.Slider) musicVolume: cc.Slider = null;
+    // 音效节点
+    @property(cc.Slider) effectVolume: cc.Slider = null;
+    // 背景音乐是否开启节点
+    @property(cc.ToggleContainer) bgm: cc.ToggleContainer = null;
+    // 游戏帧数节点
+    @property(cc.ToggleContainer) fps: cc.ToggleContainer = null;
+    // 是否初始化结束
+    initState = false;
+    
+    start() {
+        const { config } = State.system;
+        console.log(this.musicVolume);
+        this.musicVolume.progress = config.volume.music;
+        this.effectVolume.progress = config.volume.effects;
+        this.setToggleFouse(this.bgm, config.bgm);
+        this.setToggleFouse(this.fps, config.fps);
+
+        this.initState = true;
+    }
+
+
+    setToggleFouse(ToggleContainer: cc.ToggleContainer, value: any) {
+        const { children } = ToggleContainer.node;
+        for (const chi of children) {
+            if (chi._name === value) {
+                chi.getComponent(cc.Toggle).check();
+                break;
+            }
+        }
+    }
 
 
     /**
      * 修改音量时
      * @param e 参数
      */
-    volumeChang(e) {
+    MusicVolumeChang(e) {
         const { volume } = State.system.config;
-        volume.music = volume.effects = e.progress;
+        volume.music = e.progress;
+        State.system.updateSetting();
+    }
+
+
+    /**
+     * 修改音量时
+     * @param e 参数
+     */
+    EffectVolumeChang(e) {
+        const { volume } = State.system.config;
+        volume.effects = e.progress;
         State.system.updateSetting();
     }
 
@@ -41,6 +83,6 @@ export default class UserGameSetting extends cc.Component {
      */
     FpsChang(e) {
         State.system.config.fps = e.target.name;
-        State.system.updateSetting(true);
+        State.system.updateSetting(this.initState);
     }
 }
